ee4a0d45c19390eb8319e3b86c07b3737809109c
VitaoDeveloper <paulo.borgess.prates@gmail.com>
2026-06-06 13:58:23 -0300

    feat: add ApiProperties for swagger ui schemas in User endpoints

--- api/src/user/dto/create-user.dto.ts
+++ api/src/user/dto/create-user.dto.ts
@@ -1,1 +1,29 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+
+export class CreateUserDto {
+    @ApiProperty({
+        description: 'User name',
+        example: 'Ricardo Matos',
+        maxLength: 80, 
+    })
+    @IsString()
+    name: string;
+
+    @ApiProperty({
+        description: 'User email',
+        example: 'rick.mc@gmail.com',
+        format: "email",
+        maxLength: 80,
+    })
+    @IsString()
+    email: string;
+
+    @ApiProperty({
+        description: 'User LLM API Key',
+        example: 'llmy_qWlfaBRSWG65fsRvVLVHf6JxnjOnHu6UqpFS45fyUcuVk7OSEyl7yo6H',
+        maxLength: 80,
+    })
+    @IsString()
+    apiKey: string;
+}

90e0680fd54d0463855018d20e51f74249bd0532
VitaoDeveloper <paulo.borgess.prates@gmail.com>
2026-06-06 10:57:57 -0300

    feat: create LLM module and service

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SummaryModule } from './summary/summary.module';
+import { LlmModule } from './llm/llm.module';
 
 @Module({
-  imports: [SummaryModule],
+  imports: [SummaryModule, LlmModule],
   controllers: [AppController],
   providers: [AppService],
 })

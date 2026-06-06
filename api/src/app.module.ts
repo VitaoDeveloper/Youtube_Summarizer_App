eb0dadb7289a69f042e5c610e912852405efd47c
VitaoDeveloper <paulo.borgess.prates@gmail.com>
2026-06-06 11:12:09 -0300

    fix: import llm and prisma modules in App module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SummaryModule } from './summary/summary.module';
 import { LlmModule } from './llm/llm.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [SummaryModule, LlmModule],
+  imports: [
+    SummaryModule, 
+    LlmModule, 
+    PrismaModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

a9d5ea6d82630636cb3c8b9be195c357aafab9f3
VitaoDeveloper <paulo.borgess.prates@gmail.com>
2026-06-06 14:04:21 -0300

    feat: add ApiProperties for swagger ui schemas in summary endpoints

--- api/src/summary/dto/create-summary.dto.ts
+++ api/src/summary/dto/create-summary.dto.ts
@@ -1,1 +1,33 @@
-export class CreateSummaryDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
+export class CreateSummaryDto {
+  @ApiProperty({
+    description: 'Video URL',
+    example: 'https://youtube.com/watch?v=...'
+  })
+  @IsString()
+  url: string;
+
+  @ApiProperty({
+    description: 'UserId related to summary',
+    format: 'uuid'
+  })
+  @IsString()
+  userId: string;
+
+  @ApiProperty({
+    description: 'Summary language',
+    example: 'en',
+    maxLength: 2
+  })
+  @IsString()
+  language: string;
+
+  @ApiProperty({
+    description: 'Video length',
+    example: 'medium'
+  })
+  @IsString()
+  length: string;
+}

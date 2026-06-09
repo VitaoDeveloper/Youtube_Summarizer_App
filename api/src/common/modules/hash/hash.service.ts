5444a2d54e0644c620cfb4fdb95da834aed960c2
VitaoDeveloper <paulo.borges.prates@gmail.com>
2026-06-09 11:17:22 -0300

    feat: create HashService functions for argon2Id encryptation

--- api/src/common/modules/hash/hash.service.ts
+++ api/src/common/modules/hash/hash.service.ts
@@ -1,4 +1,24 @@
+import { argon2id, hash, verify, Options } from 'argon2';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class HashService {}
+export class HashService {
+    async hash(text: string): Promise<string> {
+        const options: Options = {
+            type: argon2id,
+            memoryCost: 65536,
+            timeCost: 3,
+            parallelism: 2
+        };
+
+        const hashed = await hash(text, options);
+
+        return hashed;
+    }
+
+    async verify(hash: string, text: string): Promise<boolean> {
+        const validation = await verify(hash, text);
+        
+        return validation;
+    }
+}

8702f4188c129289c3fbc001912a36b326784b82
VitaoDeveloper <paulo.borges.prates@gmail.com>
2026-06-09 14:05:57 -0300

    feat: create userExists function to avoid double database calls

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,14 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { AuthLoginDto } from './dto/auth-login.dto';
 import { PrismaService } from '../common/modules/prisma/prisma.service';
+import { HashService } from 'src/common/modules/hash/hash.service';
+import { User } from 'generated/prisma/client';
 
 @Injectable()
 export class AuthService {
-  constructor (private prisma: PrismaService) {}
+  constructor (
+    private prisma: PrismaService,
+    private validation: HashService
+  ) {}
+
+  private async userExists(email: string): Promise<{ exists: boolean, hash: Partial<User> | null }> {
+    const hash = await this.prisma.user.findUnique({ 
+      where: { email },
+      select: { password: true } 
+    });
+
+    const exists = hash ? true : false;
+
+    return {
+      exists,
+      hash
+    };
+  }
 
   async login(dto: AuthLoginDto) {
+    //const user: boolean = await this.userExists(dto.email) ? true : false;
 
+    if (1==1) {
+      //const validate = this.validation.verify(, dto.password) 
+    }
 
-    return await 'This action adds a new auth';
+    return "user";
   }
 }

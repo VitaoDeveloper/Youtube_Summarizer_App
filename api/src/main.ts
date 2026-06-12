6f15803b5ef0c6ff268172bc4aa596d33a34c256
Paulo Vitor <paulo.borges.prates@gmail.com>
2026-06-12 08:11:13 -0300

    feat: enable CORS in main.ts

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,6 +7,17 @@ async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT|| 3000;
 
+  app.enableCors({
+    origin: [
+      'https://yt-summarizer-henna.vercel.app/', 
+      'http://localhost:5173'
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
+
   await setupSwagger(app);
   
   await app.listen(port);
